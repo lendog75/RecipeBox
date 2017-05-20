c96b87417a513b2affae033c98ee780dd656c3c1
Leonard Reed <leonardreed@Leonards-MacBook-Pro.local>
2017-05-19 19:54:44 -0600

    initial setup for recipe service

---
+++ src/app/providers/recipe.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class RecipeService {
+
+  constructor() {
+
+  }
+}
+
