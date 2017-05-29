2b9d0648ddb42a5a7e03ab2c5a8df7a3f755b665
Leonard Reed <leonardreed@Leonards-MacBook-Pro.local>
2017-05-29 12:36:01 -0600

    add CategoryDetailComponent, restructure HomeComponent

---
+++ src/app/category/category-detail/category-detail.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../shared/model/category';
+import { CategoryService } from '../../providers/category.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'category-detail',
+  templateUrl: './category-detail.component.html',
+  styleUrls: ['./category-detail.component.scss']
+})
+export class CategoryDetailComponent implements OnInit {
+  category: Category;
+  id: string;
+
+  constructor (private categorySvc: CategoryService,
+               private route: ActivatedRoute) { }
+
+  ngOnInit () {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+
+      this.categorySvc.getCategory(this.id).subscribe(x => {
+        this.category = x;
+      });
+    });
+  }
+
+}
--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CategoryEditComponent } from './category-edit/category-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CategoryDetailComponent } from './category-detail/category-detail.component';
+import { RecipeModule } from '../recipe/recipe.module';
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
-  CategoryEditComponent
+  CategoryEditComponent,
+  CategoryDetailComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    RouterModule,
+    RecipeModule
 
   ],
   exports: [
