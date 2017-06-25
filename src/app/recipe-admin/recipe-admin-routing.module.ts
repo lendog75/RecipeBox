468d6a027890d3b54ea2e9f5211e1bc340531cdd
Leonard Reed <leonardreed@Leonards-MacBook-Pro.local>
2017-06-24 18:22:42 -0600

    still more responsive, and ui tweaks

--- src/app/recipe-admin/recipe-admin-routing.module.ts
+++ src/app/recipe-admin/recipe-admin-routing.module.ts
@@ -1,16 +1,19 @@
-
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import { AuthGuard } from '../shared/providers/auth-guard.service';
 import { RecipeEditContainerComponent } from './recipe-edit-container/recipe-edit-container.component';
 
-
 const routes: Routes = [
-  { path: 'recipes/0/create', component: RecipeEditContainerComponent, canActivate: [ AuthGuard ] },
-  { path: 'recipes/:id/edit', component: RecipeEditContainerComponent, canActivate: [ AuthGuard ] },
+  { path: 'recipes/0/create',
+    component: RecipeEditContainerComponent,
+    canActivate: [ AuthGuard ]
+  },
+  { path: 'recipes/:id/edit',
+    component: RecipeEditContainerComponent,
+    canActivate: [ AuthGuard ]
+  },
 ];
-
 
 @NgModule({
   imports: [
